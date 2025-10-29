d724b7ddaeed0d1ebb23e5b55b62c249c6aa8405
pabloleon2005 <pabloguaman2005@gmail.com>
2025-10-29 12:16:24 -0500

    septimo commit

---
+++ Ejemplo01/src/ejemplo01/Ejemplo051.java
@@ -0,0 +1,34 @@
+/*
+René Rolando
+
+Elizalde Solano
+
+1983
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo051 {
+    public static void main(String[] args) {
+        // Secuencia de escape \n y \t
+        String nombreEstudiante = "René Rolando";
+        String apellidoEstudiante = "Elizalde Solano";
+        int nacimiento = 1983;
+        
+        System.out.println(nombreEstudiante+"\n"+apellidoEstudiante+ "\n"+ 
+                nacimiento);
+        
+        System.out.println(nombreEstudiante+"\n\n"+apellidoEstudiante+ "\n\n"+ 
+                nacimiento);
+        
+        System.out.println(nombreEstudiante+"\n"+"\n"+apellidoEstudiante+ "\n"+
+                "\n"+ nacimiento); 
+        //System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
+        //System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante);   
+        // El \t crea un TAB es decir genera un tabulador antes de leer la variable
+        // El \n hace un salto de linea, es decir pasa a la siguiente linea de codigo
+    }
+}
