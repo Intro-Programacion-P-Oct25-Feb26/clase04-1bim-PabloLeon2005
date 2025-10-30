fb285b42660f7841d298698fe0e3fb0c69b9146e
PabloLeon2005 <pabloguaman2005@gmail.com>
2025-10-30 10:38:45 -0500

    Cuarto commit Jueves

--- Ejemplo02/src/paquete1/Ejemplo11.java
+++ Ejemplo02/src/paquete1/Ejemplo11.java
@@ -36,7 +36,7 @@ public class Ejemplo11 {
         notaAsignatura1 = entrada.nextDouble();
         
         
-        System.out.printf("Asignatura 1: %s\nNota 1:%.2f",
+        System.out.printf("Asignatura 1: %s\nNota 1: %.1f",
                 nombreAsignatura1,
                 notaAsignatura1
                 );
